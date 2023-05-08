cad78bd3c68fb2c98e5fc5a3399ea3363789c90d
Dylan Fontaine <me@dylf.dev>
2023-05-07 18:51:42 -0700

    add newsletter publish email functionality

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -65,6 +65,16 @@ impl TestApp {
             .await
             .expect("Failed to execute request.")
     }
+
+    pub async fn post_newsletters(&self, body: serde_json::Value) -> reqwest::Response {
+        reqwest::Client::new()
+            .post(&format!("{}/newsletters", &self.address))
+            .header("Content-Type", "application/json")
+            .json(&body)
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
 }
 
 pub async fn spawn_app() -> TestApp {
